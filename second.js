6e291a23cc03e64ea631099f2fce65fdb3ac528f
kidfella <andrechapina51@aragon.unam.mx>
2023-03-31 21:36:11 -0600

    second

---
+++ second.js
@@ -0,0 +1,15 @@
+// 1. Calculadora
+
+function calculator(num1, num2, operator) {
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*') {
+    return num1 * num2;
+  } else if (operator === '/') {
+    return num1 / num2;
+  } else {
+    return 'Invalid operator';
+  }
+}
